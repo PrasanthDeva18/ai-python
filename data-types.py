ed5917b56ded55d4994416a628e3ae45b7f21a1a
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-09-29 12:11:11 +0530

    Create data-types.py

---
+++ data-types.py
@@ -0,0 +1,18 @@
+# Types 
+
+# text type -> str
+# numeric type -> int, float, complex
+# sequence type -> list - [], range - range(), tuple - ()
+# mapping type -> dict - {key : value}
+# boolean type -> bool
+
+a = "prasanth"
+b = 5
+c = 15.4
+d = 1j
+e = ["hello", "world"] #heterogeneous
+f = ("hello" , 5) #heterogeneous
+g = range(7) # range(0 , 7)
+h = False # True, False
+
+print(a, b,  c, d , e ,f, g , h)
