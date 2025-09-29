09dc857d37808d8cf66a22c6f797ac35457204bd
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-09-29 11:49:49 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+x = 5 #int
+y = "prasanth" #string
+z = "vennila"
+#single or double quoted value consider as string
+z1 = 'deva' #string
+z2 = 15.4333333 #float
+
+print(type(z2))
+
+
+#casting int(), str(), float()
