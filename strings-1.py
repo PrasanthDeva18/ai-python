7c33e6bd43a9ee4be4adf2b38b1faabff0117084
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-09-29 12:31:40 +0530

    Create strings-1.py

---
+++ strings-1.py
@@ -0,0 +1,40 @@
+# strings
+
+# single line strings
+
+a11 = "prasanth"
+a1 = """ hello world, i hate python programming beacuse of its very simple """
+print(a1)
+
+# access a string value based on index
+print(a11[0])
+
+#iteration in string 
+for i in a11:
+    print(i)
+
+#find out length of the sttring
+print(len(a11))
+
+#check the string is available or not
+print("p" in a11)
+
+#using if
+if "p" in a11:
+    print("true")
+
+#using not 
+print("p" not in a11)
+#using if
+if "p" not in a11:
+    print("true")
+else:
+    print("false")
+
+
+
+a = "prasanth"
+b = "Prasanth"
+c = 'Prasanth '
+
+print(a)
