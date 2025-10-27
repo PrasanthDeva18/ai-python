a62bd9bc16ac5aa05d7fa83425ad5d86dbeb748c
prasanth <prasanthdeva18@gmail.com>
2025-10-27 15:10:39 +0530

    dic and set learned

---
+++ dict/dict.py
@@ -0,0 +1,17 @@
+a = {"name" : "prasanth"}
+
+print(a["name"])
+
+#same as js
+
+
+#loop
+
+b = {
+    "name" : "prasanth",
+    "class" : "IT",
+    "roll number" : 80
+}
+
+for key, value in b.items():
+    print(f"{key}, {value}")
---
+++ sets/sets.py
@@ -0,0 +1,15 @@
+a = {1,2,3,4,4}
+
+# set is unordered, unindexed, unchangeable - not allow duplicates
+print(a) #{1,2,3,4} automatically remove duplicates
+
+a.add(5) #{1,2,3,4,5}
+a.remove(1) #{2,3,4}
+
+
+# set operations - &, | , -, ^
+
+for i in a:
+    print(i)
+
+    
