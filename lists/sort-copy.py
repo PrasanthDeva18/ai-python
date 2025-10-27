ababad52df77e68523e76c1103ecf15770ac0996
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-10-27 14:18:24 +0530

    Create sort-copy.py for list operations
    Add a script to demonstrate list sorting and copying.

---
+++ lists/sort-copy.py
@@ -0,0 +1,14 @@
+a = [1,2,3,5] 
+# a.sort() #[1,2,3,5] 
+# a.sort(reverse = True) #[5, 3, 2, 1]
+# a.reverse() #[5, 3, 2, 1]
+# print(a)
+
+#copy
+# b = a.copy()
+
+# c = list(a)
+d=a[:]
+# print(b)
+# print(c)
+print(d)
